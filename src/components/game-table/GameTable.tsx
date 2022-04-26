2019ae463e528916410d808206b946bb03617530
Shawn Bahramy <shawnbahramy@gmail.com>
2022-04-26 14:27:29 -0400

    very basic card playing functionality

--- src/components/game-table/GameTable.tsx
+++ src/components/game-table/GameTable.tsx
@@ -1,20 +1,61 @@
+import { useState } from "react"
 import "./gameTable.css"
+
+type cards = (number | string)[];
+
+interface IState {
+  opponentHand: cards,
+  board: cards,
+  playerHand: cards
+}
+
+const initialState: IState = {
+  opponentHand: [1, 3, 5],
+  board: Array(4).fill('_'),
+  playerHand: [2, 4, 6]
+}
 
 
 export const GameTable = () => {
 
+  const [state, setState] = useState(initialState);
+
+  const onClick = (handType: 'opponentHand' | 'playerHand') => {
+    const playableCardIndex = findPlayableCardIndex(state[handType]);
+    const emptyBoardCellIndex = findEmptyBoardCellIndex(state.board);
+    
+    if (playableCardIndex < 0 || emptyBoardCellIndex < 0)
+      return;
+
+    const newHand = [...state[handType]];
+    const newBoard = [...state.board];
+    newHand[playableCardIndex] = 'X';
+    newBoard[emptyBoardCellIndex] = state[handType][playableCardIndex];
+    setState({...state, board: newBoard, [handType]: newHand})
+  }
+
+  const findPlayableCardIndex = (hand: cards): number => {
+    return hand.findIndex(card => card !== 'X');
+  }
+
+  const findEmptyBoardCellIndex = (board: cards): number => {
+    return board.findIndex(cell => cell === '_');
+  }
+
   return (
     <div className="game-table">
       <div className="side-panel">
-        opponent hand
+        {state.opponentHand}
+        <button onClick={() => onClick('opponentHand')}>Play Card</button>
       </div>
 
       <div className="board-container">
-        board
+        {state.board}
       </div>
 
       <div className="side-panel">
-        player hand
+        {state.playerHand}
+        <button onClick={() => onClick('playerHand')}>Play Card</button>
       </div>
     </div>
   )
--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -2,7 +2,7 @@ import { ReactNode }  from "react";
 import './layout.css';
 
 
-export const Layout = (props: { children: ReactNode}) => {
+export const Layout = (props: { children: ReactNode }) => {
 
     return (
         <div className="layout">
